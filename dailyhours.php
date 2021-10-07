e54a3cfe4213ad1fd75537e29473311aea351f5a
Stacey Knight-Davis <slknight@eiu.edu>
2021-10-07 14:14:11 -0500

    Update dailyhours.php
    The widget version stopped working in 10/2021. I re-wrote it as a real API call. Hopefully this will be more stable

--- dailyhours.php
+++ dailyhours.php
@@ -1,77 +1,202 @@
 <?php 
 
 
-$url = "https://YOURLIBRARY.libcal.com/widget/hours/grid?iid=YOURID&format=json&weeks=2&systemTime=0";
 
 
-
-$json = file_get_contents($url);
-$result = json_decode($json, true);
-
-//this should be obvious, but you must have hours filled in it LibCal to get data
-
-//troubleshooting to see what is in the XML
-//var_dump($result);
+//error reporting for testing. uncomment next line to see errors
+//ini_set('error_reporting', E_ALL);
 
 
+//setting dates in correct format for today and next 2 days
+//You can make this run for more days by making more variables for more days
+
+	$today= date('Y-m-d');
+		
+	$tomorrow= date('Y-m-d', strtotime($today. ' + 1 days'));
+
+	$enddate= date('Y-m-d', strtotime($today. ' + 2 days'));
+
+	
+//setting day of week for next two days
+
+$tomorrowday= date('l', strtotime($today. ' + 1 days'));
+	
+
+	
+	$twodaysout = date('l', strtotime($today. ' + 2 days'));
+
+//using 1.1 API
+
+$token_url = "https://eiu.libcal.com/1.1/oauth/token";
+
+//replace Xs with the location ID you want hours for
+
+$test_api_url = "https://eiu.libcal.com/api/1.1/hours/XXXXXXX?&from=$today&to=$enddate";
 
 
-//echo any of these vars to see rendered hours for that day of the week
+//	client (application) credentials. Find/create this in libcal admin under API 
+$client_id = "XXX";
+$client_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 
+function getAccessToken() {
+	global $token_url, $client_id, $client_secret;
 
-$sunday=$result['locations'][0]['weeks'][0]['Sunday']['rendered'];
-$monday=$result['locations'][0]['weeks'][0]['Monday']['rendered'];
-$tuesday=$result['locations'][0]['weeks'][0]['Tuesday']['rendered'];
-$wednesday=$result['locations'][0]['weeks'][0]['Wednesday']['rendered'];
-$thursday=$result['locations'][0]['weeks'][0]['Thursday']['rendered'];
-$friday=$result['locations'][0]['weeks'][0]['Friday']['rendered'];
-$saturday=$result['locations'][0]['weeks'][0]['Saturday']['rendered'];
+	$content = "grant_type=client_credentials";
+	$authorization = base64_encode("$client_id:$client_secret");
+	$header = array("Authorization: Basic {$authorization}","Content-Type: application/x-www-form-urlencoded");
 
-$today=date("l");
-$date=date("Y-m-d");
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $token_url,
+		CURLOPT_HTTPHEADER => $header,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => $content
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
 
-
-if ($result['locations'][0]['weeks'][0][$today]['date']==$date)
-{
-echo "<ul><li>Today: <span class=\"smaller\">" . $result['locations'][0]['weeks'][0][$today]['rendered'] . "</span></li>";}
-
-//loop is needed in case today is saturday and tomrrow is week[1]
-
-  $tomorrow = date("l", strtotime("+1 day"));
-$date2 = date("Y-m-d",  strtotime("+1 day"));
-
-
-
-
-
-for($i=0; $i<=1; $i++) {
-
-if ($result['locations'][0]['weeks'][$i][$tomorrow]['date']==$date2)
-{
-
-
-echo "<li>" . $tomorrow . ":  <span class=\"smaller\">" . $result['locations'][0]['weeks'][$i][$tomorrow]['rendered'] . "<span></li>";
+	return json_decode($response)->access_token;
 }
 
-}
-
-$twodaysout = date("l", strtotime("+2 day"));
-
-$date3 = date("Y-m-d",  strtotime("+2 day"));
-
-for($j=0; $j<=1; $j++) {
-
-if ($result['locations'][0]['weeks'][$j][$twodaysout]['date']==$date3)
-{
+	$access_token = getAccessToken();
 
 
- echo "<li>" . $twodaysout . ":  <span class=\"smaller\">" . $result['locations'][0]['weeks'][$j][$twodaysout]['rendered'] . "</span></li> </ul>";
-}
 
 
-}
 
+	$header = array("Authorization: Bearer {$access_token}");
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => $test_api_url,
+		CURLOPT_HTTPHEADER => $header,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_RETURNTRANSFER => true
+	));
+	$response = curl_exec($curl);
+	if(!$response){die("Connection Failure");}
+	
+
+	
+
+	
+	
+   curl_close($curl);
+   
+//for testing. Uncomment to see ray JSON
+//echo $response;
+   
+	
+    $array = json_decode($response, true);
+ 
+//uncomment this to see the array
+// print_r($array);     
+  
+
+
+
+echo "<p><ul><li>Today: <span class=\"smaller\"> ";
+	
+
+$status= $array[0]['dates'][$today]['status'];
+
+//check if status not open
+
+if ($status!=="open")
+	
+{echo "Closed";}
+
+//echo hours 
+
+else
+	{
+	
+	foreach($array[0]['dates'][$today]['hours']as $key => $value){
+			
+			
+			
+		
+				
+				$from=$value['from'];
+					
+				$to=$value['to'];
+			if ($to=="12:00am")
+			{$to="Midnight";}
+			
+			
+			echo $from;
+	echo "-";
+		echo $to;}}
+	
+	echo "</span></li><li>";
+	
+	
+	echo $tomorrowday;
+	echo ": <span class=\"smaller\">";
+
+$status= $array[0]['dates'][$tomorrow]['status'];
+
+if ($status!=="open")
+	
+{echo "Closed";}
+
+else
+	{
+	
+	foreach($array[0]['dates'][$tomorrow]['hours']as $key => $value){
+			
+				
+		
+				
+				$from=$value['from'];
+					
+				$to=$value['to'];
+			if ($to=="12:00am")
+			{$to="Midnight";}
+			
+			
+			echo $from;
+	echo "-";
+		echo $to;
+	
+	
+	}	}
+
+	
+	echo "</span></li><li>";
+	echo $twodaysout;
+	echo ": <span class=\"smaller\">";
+
+$status= $array[0]['dates'][$enddate]['status'];
+
+if ($status!=="open")
+	
+{echo "Closed";}
+
+else
+	{
+	
+	foreach($array[0]['dates'][$enddate]['hours']as $key => $value){
+			
+			
+			
+		
+				
+				$from=$value['from'];
+					
+				$to=$value['to'];
+			if ($to=="12:00am")
+			{$to="Midnight";}
+			
+			
+			echo $from;
+	echo "-";
+		echo $to;}
+	echo "</span></li>";
+	echo "</ul></p>";}	
 
 ?>
 
